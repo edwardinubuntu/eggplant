6a15b9d8a15c8c3e764c7d96b5100b07d6a71117
Edward Chiang <edward@thepolydice.com>
2012-10-21 00:47:49 +0800

    New cell design

--- eggplant/classes/EPWikiCell.m
+++ eggplant/classes/EPWikiCell.m
@@ -10,4 +10,34 @@
 
 @implementation EPWikiCell
 
+#define kCellHeight 440
+
+- (void)layoutSubviews {
+  [super layoutSubviews];
+  
+  CGFloat imageSize = self.contentView.frame.size.width - 10 * 2;
+  self.imageView.frame = CGRectMake(10, 10, imageSize, imageSize);
+  
+  CGFloat width = self.contentView.frame.size.width - 10 * 2;
+  CGFloat left = 10;
+  self.textLabel.backgroundColor = [UIColor clearColor];
+  self.textLabel.frame = CGRectMake(left, self.imageView.frame.size.height + self.imageView.frame.origin.y + 5, width, self.textLabel.frame.size.height);
+  
+  self.detailTextLabel.backgroundColor = [UIColor clearColor];
+  self.detailTextLabel.frame = CGRectMake(left, self.textLabel.frame.origin.y + 20, width, 65);
+  
+  self.sourceLabel.frame = CGRectMake(left, self.detailTextLabel.frame.origin.y + self.detailTextLabel.frame.size.height, width, 16.f);
+}
+
++ (CGFloat)cellHeight {
+  return kCellHeight;
+}
+
++ (CGFloat)cellHeight:(NSString *)text detail:(NSString *)detail {
+  if (!NIIsStringWithAnyText(detail)) {
+    return kCellHeight -60;
+  }
+  return kCellHeight;
+}
+
 @end
