c5bc5d717362a769aff77ca4d84b7b9df9cec757
Edward Chiang <edward@thepolydice.com>
2012-10-21 06:40:30 +0800

    Adjust scroll speed

--- eggplant/classes/EPHomeViewController.m
+++ eggplant/classes/EPHomeViewController.m
@@ -64,6 +64,7 @@ CGFloat smallMoving = 25;
   self.searchKeywordViewController.delegate = self;
   
   _headerCarousel = [[iCarousel alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 44.f)];
+  self.headerCarousel.scrollSpeed = 1;
   self.headerCarousel.delegate = self;
   self.headerCarousel.dataSource = self;
   self.headerCarousel.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"navy_blue_"]];
